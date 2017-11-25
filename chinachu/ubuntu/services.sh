85fc965f699a427974ca62ef0a1f705be0fc40a2
Yusei Yamanaka <yusei1128@gmail.com>
2017-11-25 11:21:03 +0900

    Works on synology NAS

--- chinachu/ubuntu/services.sh
+++ chinachu/ubuntu/services.sh
@@ -9,16 +9,13 @@ fi
 rm -f /var/run/chinachu-operator.pid > /dev/null 2>&1
 rm -f /var/run/chinachu-wui.pid > /dev/null 2>&1
 
-/etc/init.d/chinachu-operator start
-/etc/init.d/chinachu-wui start
-
-apt update \
-	&& apt install -y build-essential python \
-	&& cd /usr/local/chinachu \
-	&& sudo -H -u chinachu PATH=/usr/local/chinachu/.nave/installed/6.9.2/bin:$PATH npm install diskusage@latest # workaround
+cd /usr/local/chinachu && sudo -H -u chinachu bash -c "PATH=/usr/local/chinachu/.nave/installed/6.9.2/bin:$PATH npm install diskusage@latest" # workaround
 chown chinachu:chinachu /usr/local/chinachu/recorded
 chown chinachu:chinachu /usr/local/chinachu/data
 chown chinachu:chinachu /usr/local/chinachu/config.json
 chown chinachu:chinachu /usr/local/chinachu/rules.json
+
+/etc/init.d/chinachu-operator start
+/etc/init.d/chinachu-wui start
 
 tail -f /dev/null
